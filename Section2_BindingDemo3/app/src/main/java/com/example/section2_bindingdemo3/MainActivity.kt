1b026b65cf1601bd29469814bef2203d9c7f71d3
jegan <jeganpp@gmail.com>
2021-06-13 11:24:21 -0400

    added student object and bound it to the layout

--- Section2_BindingDemo3/app/src/main/java/com/example/section2_bindingdemo3/MainActivity.kt
+++ Section2_BindingDemo3/app/src/main/java/com/example/section2_bindingdemo3/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.section2_bindingdemo3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.section2_bindingdemo3.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.apply {
+            student = getStudentData();
+            button.setOnClickListener {
+                student = Student(2, "Dharshini", "dharshini@gmail.com")
+            }
+        }
+
+    }
+
+    private fun getStudentData() : Student {
+        return Student(1, "Akshith", "akshith@gmail.com")
     }
 }
---
+++ Section2_BindingDemo3/app/src/main/java/com/example/section2_bindingdemo3/Student.kt
@@ -0,0 +1,7 @@
+package com.example.section2_bindingdemo3
+
+data class Student(
+    var id : Int,
+    var name : String,
+    var emailId : String
+)
